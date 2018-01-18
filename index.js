d09762d5a94b51b1692b571991b6c8eb0194bfa8
Melissa Real <melissa.real@hotmail.com>
2018-01-18 13:55:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicianArr, instrumentArr){
+  
+  var emptyArrYay = [];
+  
+  for(let i = musicianArr.length; i > 0; i--){
+    
+      emptyArrYay.push(...musicianArr[i] + 'plays' + ...instrumentArr[i])
+    }
+    
+    return emptyArrYay;
+}
+
+
+function johnLennonFacts(jFacto){
+  
+  var i = 0;
+
+  while(i<jFacto.length){
+    
+  jFacto[i] += '!!!';
+  i++;
+  
+    
+  }
+  return jFacto;
+}
